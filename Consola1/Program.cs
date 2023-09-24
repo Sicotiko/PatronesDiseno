7968d39be7ae0f07cd1a86cd78bce85a9120b40d
elxpi <elxpi@ANDRES>
2023-09-23 22:50:49 -0300

    Agregar archivos de proyecto.

---
+++ Consola1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Consola1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PruebaSingleton();
+        }
+
+        private static void PruebaSingleton()
+        {
+            Console.WriteLine($"Primera Prueba {Singleton.Singleton.Instance.NumeroRandom}");
+            Console.WriteLine($"Segunda Prueba {Singleton.Singleton.Instance.NumeroRandom}");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,24 @@
+﻿namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton instance = null;
+        public string Mensaje = string.Empty;
+        public int NumeroRandom = 0;
+        protected Singleton()
+        {
+            Mensaje = "Hola Mundo!";
+            NumeroRandom = new Random().Next();
+        }
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+
+                return instance;
+            }
+        }
+    }
+}
