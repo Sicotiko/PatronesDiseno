3f1fc403252dd34c94c34cddfbf138075717ee7d
Andres <elmer.a.morales.v@gmail.com>
2023-09-23 23:14:57 -0300

    PrototypeDeep

---
+++ PrototypeDeep/Documento.cs
@@ -0,0 +1,19 @@
+﻿namespace PrototypeDeep
+{
+    public class Documento : ICloneable
+    {
+        public string NombreDocumento { get; set; } = string.Empty;
+        public int CantidadHojas { get; set; }
+
+        public Documento(string nombreDocumento, int cantidadHojas)
+        {
+            NombreDocumento = nombreDocumento;
+            CantidadHojas = cantidadHojas;
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
---
+++ PrototypeDeep/Portafolio.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace PrototypeDeep
+{
+    public class Portafolio : ICloneable
+    {
+        public string Color { get; set; } = string.Empty;
+        public int Capacidad { get; set; }
+        public Documento Documento { get; set; }
+
+        public Portafolio(string color, int capacidad)
+        {
+            Color = color;
+            Capacidad = capacidad;
+            Documento = new Documento("Guia 1", 3);
+        }
+
+        public object Clone()
+        {
+            Portafolio portafolio = (Portafolio)this.MemberwiseClone();
+            portafolio.Documento = (Documento)Documento.Clone();
+            return portafolio;
+        }
+    }
+}
