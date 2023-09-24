89e643151d103a22a7a48b7994595d161ac6c2b5
Andres <elmer.a.morales.v@gmail.com>
2023-09-23 23:14:48 -0300

    ProtoypeShallow

---
+++ PrototypeShallow/Portafolio.cs
@@ -0,0 +1,19 @@
+﻿namespace PrototypeShallow
+{
+    public class Portafolio : ICloneable
+    {
+        public string Color { get; set; } = string.Empty;
+        public int Capacidad { get; set; }
+
+        public Portafolio(string color, int capacidad)
+        {
+            Color = color;
+            Capacidad = capacidad;
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
